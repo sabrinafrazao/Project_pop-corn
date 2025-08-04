fafab3386d1f663e83b1af220cc786501b602513
NelsonThiago <2022004387@ifam.edu.br>
2025-08-04 18:55:35 -0400

    feat(admin): adicionar componentes standalone para gestão de filmes e sessões

---
+++ pop_corn/src/app/admin/movie-management.component/movie-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-management.component',
+  imports: [],
+  templateUrl: './movie-management.component.html',
+  styleUrl: './movie-management.component.scss'
+})
+export class MovieManagementComponent {
+
+}
---
+++ pop_corn/src/app/admin/session-management.component/session-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-management.component',
+  imports: [],
+  templateUrl: './session-management.component.html',
+  styleUrl: './session-management.component.scss'
+})
+export class SessionManagementComponent {
+
+}
