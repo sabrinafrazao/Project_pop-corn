a11bd5c47276ceaeb2045da637fb4e8465eea3cc
NelsonThiago <2022004387@ifam.edu.br>
2025-08-06 06:29:24 -0400

    refactor(services): Alinha os serviços de autenticação e cinema com as novas funcionalidades

--- pop_corn/src/app/auth/services/abstract-auth.service.ts
+++ pop_corn/src/app/auth/services/abstract-auth.service.ts
@@ -1,5 +1,7 @@
 import { User } from '../models/user.model';
 import { Signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OperationResult } from '../../models/operation-result.model';
 
 export abstract class AbstractAuthService {
   abstract currentUser: Signal<User | null>;
@@ -7,6 +9,10 @@ export abstract class AbstractAuthService {
   abstract isAdmin: Signal<boolean>;
   abstract isMaster: Signal<boolean>;
 
+  abstract allUsers: Signal<User[]>;
+  abstract updateUser(user: User): Observable<OperationResult>;
+  abstract addUser(userData: Omit<User, 'id'>): Observable<OperationResult<User>>;
+  
   abstract login(email: string, password: string): void;
   abstract logout(): void;
 }
--- pop_corn/src/app/cinemas/service/abstract-cinema.service.ts
+++ pop_corn/src/app/cinemas/service/abstract-cinema.service.ts
@@ -7,12 +7,14 @@ import { Room } from "../models/room.model";
 
 export abstract class AbstractCinemaService {
   abstract cinemas: Signal<Cinema[]>;
+  // CORREÇÃO: O método DEVE devolver a estrutura de Cinemas com as sessões filtradas.
   abstract loadSessionsByMovie(movieId: number): Observable<Cinema[]>;
   abstract getSessionDetails(sessionId: number): Observable<Session | undefined>;
   abstract addCinema(cinema: Omit<Cinema, 'id' | 'rooms'>): Observable<OperationResult>;
   abstract updateCinema(cinema: Cinema): Observable<OperationResult>;
   abstract deleteCinema(cinemaId: number): Observable<OperationResult>;
   abstract addRoom(cinemaId: number, room: Omit<Room, 'id' | 'sessions'>): Observable<OperationResult<Room>>;
+  abstract updateRoom(cinemaId: number, room: Room): Observable<OperationResult<Room>>;
   abstract deleteRoom(cinemaId: number, roomId: number): Observable<OperationResult<void>>;
   abstract addSession(cinemaId: number, roomId: number, session: Omit<Session, 'id' | 'seatMap'>): Observable<OperationResult<Session>>;
   abstract deleteSession(cinemaId: number, roomId: number, sessionId: number): Observable<OperationResult<void>>;
